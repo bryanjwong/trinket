f74857d2cc4df1f3bc6200ba7757dbf2bd77c721
Caleb Terrill <>
2022-01-14 22:56:34 -0800

    Create esp32 folder

---
+++ esp32/trinket/trinket.ino
@@ -0,0 +1,19 @@
+#include "WiFi.h"
+#include <FirebaseESP32.h>
+
+#define FIREBASE_HOST "#########################################"
+#define FIREBASE_AUTH "##############################"
+#define SSID "##############"
+#define PASSWORD "##############"
+
+FirebaseData firebaseData;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
