c8be6bb6e9717a3aa047afa5477ddb9c35e4cca4
Bryan Wong <bryanjwong@g.ucla.edu>
2022-01-15 13:24:06 -0800

    implemented trinket level up

--- functions/index.js
+++ functions/index.js
@@ -6,30 +6,62 @@ admin.initializeApp();
 const objectives = require('./objectives.json'); 
 
 function getRandomObjective() {
-  return objectives[Math.floor(Math.random()*objectives.length)];
+  var obj = objectives[Math.floor(Math.random()*objectives.length)];
+  obj["completed"] = false;
+  return obj;
 }
 
 exports.handleObjWrite = functions.database.ref("/users/{usr}/objectives/{obj}/")
-    .onWrite((change, context) => {
-      promises = [];
-      const obj = change.after.val();
-      functions.logger.log(context.params.usr, context.params.obj);
-      functions.logger.log(obj);
+  .onWrite((change, context) => {
+    promises = [];
+    const obj = change.after.val();
+    functions.logger.log(context.params.usr, context.params.obj);
+    functions.logger.log(obj);
 
-      const type = obj["type"];
+    // Objective is complete
+    if (obj["completed"] === true) {
 
-      // Objective is complete
-      if ((type === "ge" && obj["current"] >= obj["goal"]) || 
-          (type === "le" && obj["current"] <= obj["goal"])) {
-
-        functions.logger.log(context.params.obj, "completed");
-
-        // Copy completed objective to completed objectives list
-        promises.push(admin.database().ref("/users/"+context.params.usr+"/completed_objectives/").push(obj));
-
-        // Replace completed objective with new objective
-        promises.push(change.after.ref.parent.child(context.params.obj).set(getRandomObjective()));
+      functions.logger.log(context.params.obj, "completed");
+      obj["completion_time"] = Date.now();
+      
+      // Handle reward
+      const reward = obj["reward"];
+      if (reward["type"] === "exp") {
+        promises.push(admin.database()
+                            .ref("/users/"+context.params.usr+"/active_trinket/currExp")
+                            .set(admin.database.ServerValue.increment(reward["value"])));
       }
 
-      return Promise.all(promises);
-    });
+      // Copy completed objective to completed objectives list
+      promises.push(admin.database().ref("/users/"+context.params.usr+"/completed_objectives/").push(obj));
+
+      // Replace completed objective with new objective
+      promises.push(change.after.ref.parent.child(context.params.obj).set(getRandomObjective()));
+    }
+
+    return Promise.all(promises);
+  });
+
+exports.getNewObjs = functions.database.ref("/users/{usr}/objectives")
+  .onCreate((snap, context) => {
+    promises = [];
+    promises.push(admin.database().ref("/users/"+context.params.usr+"/objectives/obj1").set(getRandomObjective()));
+    promises.push(admin.database().ref("/users/"+context.params.usr+"/objectives/obj2").set(getRandomObjective()));
+    promises.push(admin.database().ref("/users/"+context.params.usr+"/objectives/obj3").set(getRandomObjective()));
+    return Promise.all(promises);
+  });
+
+exports.handleTrinketLevelUp = functions.database.ref("/users/{usr}/active_trinket/")
+  .onWrite((change, context) => {
+    var obj = change.after.val();
+    if (obj["currExp"] < obj["goalExp"]) return;
+    
+    while (obj["currExp"] >= obj["goalExp"]) {
+      obj["currExp"] -= obj["goalExp"];
+      obj["level"]++;
+      if (obj["level"] == 5 || obj["level"] == 10) {
+        obj["evolveLevel"]++;
+      }
+    }
+    return admin.database().ref("/users/"+context.params.usr+"/active_trinket/").set(obj);
+  });
